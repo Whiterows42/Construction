6837ce3024a01338fed1fe794b70ca0833ee4d29
Ajit Mote <123333490+Whiterows42@users.noreply.github.com>
2023-12-12 09:05:26 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+var dropdown = document.getElementById("navbarDropdown");
+var dropshow = document.getElementById("dropshow");
+
+function show() {
+   dropshow[0].style.display ="block"
+}
+function hide(){
+    dropshow[0].style.display ="none";
+}
